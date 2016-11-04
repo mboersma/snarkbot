8b3614af4cb6f9b34d337d2a9adae5b1e90ca954
Matt Boersma <mboersma@deis.com>
2016-11-04 16:57:55 -0600

    ref(bin/start.sh): hide rtmbot.conf from version control

--- bin/start.sh
+++ bin/start.sh
@@ -3,6 +3,6 @@
 : ${SLACK_TOKEN:?"environment variable must be set to a valid bot access token."}
 
 # Update rtmbot.conf to include the $SLACK_TOKEN
-sed -i '' -e "s/SLACK_TOKEN:.*/SLACK_TOKEN: \"$SLACK_TOKEN\"/" rtmbot.conf
+sed -e "s/SLACK_TOKEN:.*/SLACK_TOKEN: \"$SLACK_TOKEN\"/" rtmbot.tpl > rtmbot.conf
 
 exec rtmbot
